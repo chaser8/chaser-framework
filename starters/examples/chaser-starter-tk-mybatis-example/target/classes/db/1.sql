5c9be5fd92264002be2105e0c29490ffb7b69840
yangzb <chaser8@163.com>
2021-08-04 15:29:29 +0800

    add

--- starters/examples/chaser-starter-tk-mybatis-example/target/classes/db/1.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE if not exists MALL_INFO (
-     MALL_ID int(9) AUTO_INCREMENT,
-     AGENT_ID varchar(20) ,
-     MALL_TYPE varchar(10) ,
-     MALL_NBR varchar(50) ,
-     MALL_NAME varchar(250) ,
-     PRIMARY KEY (MALL_ID)
-);
